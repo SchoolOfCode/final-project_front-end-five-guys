73fcf7bf527635a37a8cf213b49b1bce237352b7
brycen <brycensoc@gmail.com>
2022-07-25 17:01:53 +0100

    fixed object path from API but number still not returning

--- medi-check/src/Components/Patient/PrescriptionDisplay/index.js
+++ medi-check/src/Components/Patient/PrescriptionDisplay/index.js
@@ -7,31 +7,31 @@ function PrescriptionDisplay() {
   // let itemInteractions = useInteractions(data);
   const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   async function fetchData(nameArray) {
-  //     let url = 'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=';
-  //     for (let i = 0; i < nameArray.length; i++) {
-  //       let res = await fetch(
-  //         `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${nameArray[i]}`
-  //       );
-  //       let json = await res.json();
-  //       console.log(json, nameArray[i]);
-  //       url += `+${json.idGroup.rxnormId[0]}`;
-  //     }
-  //     try {
-  //       let response = await fetch(url);
-  //       let obj = await response.json();
-  //       console.log(obj.fullInteractionTypeGroup);
-  //       return obj.fullInteractionTypeGroup;
-  //     } catch (error) {
-  //       console.log(error);
-  //       return [];
-  //     }
-  //   }
-  //   let arr = fetchData(dummy);
-  //   // console.log(dummy);
-  //   setData(arr);
-  // }, []);
+  useEffect(() => {
+    async function fetchData(nameArray) {
+      let url = 'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=';
+      for (let i = 0; i < nameArray.length; i++) {
+        let res = await fetch(
+          `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${nameArray[i]}`
+        );
+        let json = await res.json();
+        console.log(json, nameArray[i]);
+        url += `+${json.idGroup.rxnormId[0]}`;
+      }
+      try {
+        let response = await fetch(url);
+        let obj = await response.json();
+        console.log(obj.fullInteractionTypeGroup[0].fullInteractionType);
+        return obj.fullInteractionTypeGroup[0].fullInteractionType;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+    }
+    let arr = fetchData(dummy);
+    // console.log(dummy);
+    setData(arr);
+  }, []);
   return <div className="hi">{data.length}</div>;
 }
 
