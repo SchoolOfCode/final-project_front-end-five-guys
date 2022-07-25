de4f113c4a5096b769903c1339ad779354d310fc
brycen <brycensoc@gmail.com>
2022-07-25 16:11:19 +0100

    wrote custom hook for fetching API

--- medi-check/src/Components/Patient/PrescriptionDisplay/Item/index.js
+++ medi-check/src/Components/Patient/PrescriptionDisplay/Item/index.js
@@ -1,2 +1,17 @@
-function Item() {}
+import { useState } from 'react';
+
+function Item(props) {
+  const [item, setItem] = useState({});
+
+  // useEffect(() => {
+
+  // });
+  return (
+    <div>
+      <div className="msg"></div>
+      <div></div>
+      <div>{}</div>
+    </div>
+  );
+}
 export default Item;
