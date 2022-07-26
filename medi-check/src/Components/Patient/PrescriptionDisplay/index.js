29d1e04742dcd1c0ecc6e94d3cf1bb43adfb9617
TomBirbeck <tombirbeck47@gmail.com>
2022-07-26 10:24:25 +0100

    showing medicine clash data

--- medi-check/src/Components/Patient/PrescriptionDisplay/index.js
+++ medi-check/src/Components/Patient/PrescriptionDisplay/index.js
@@ -6,7 +6,17 @@ function PrescriptionDisplay() {
   // let itemInteractions = useInteractions(dummy);
   let itemInteractions = testInteractions;
   console.log(itemInteractions);
-  return <div className="hi">{itemInteractions.length}</div>;
+  // console.log("rawr", itemInteractions[0].interactionPair[0].severity)
+  let i = 0
+  return (
+    <>
+  <div className="hi">{itemInteractions.length}</div>
+  <div>Medicine 1: {itemInteractions[i].minConcept[0].name}</div>
+  <div>Medicine 2: {itemInteractions[i].minConcept[1].name}</div>
+  <div>Severity of interaction: {itemInteractions[i].interactionPair[0].severity}</div>
+  <div>Description: {itemInteractions[i].interactionPair[0].description}</div>
+  </>
+  )
 }
 
 export default PrescriptionDisplay;
