492b30546f4c722009daef2118951fe7735d96de
brycen <brycensoc@gmail.com>
2022-07-26 09:25:17 +0100

    cutsom hook works

--- medi-check/src/Components/Patient/PrescriptionDisplay/index.js
+++ medi-check/src/Components/Patient/PrescriptionDisplay/index.js
@@ -1,38 +1,11 @@
-import { useEffect, useState } from 'react';
-// import useInteractions from '../../../Hooks/useInteractionsFromName';
+import useInteractions from '../../../Hooks/useInteractionsFromName';
 import dummy from './dummyData';
 //Will fetch backend to get the patient prescription names and information, then plug that into the API twice.
 //Working on functionality now, not completeness
 function PrescriptionDisplay() {
-  // let itemInteractions = useInteractions(data);
-  const [data, setData] = useState([]);
+  let itemInteractions = useInteractions(dummy);
 
-  useEffect(() => {
-    async function fetchData(nameArray) {
-      let url = 'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=';
-      for (let i = 0; i < nameArray.length; i++) {
-        let res = await fetch(
-          `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${nameArray[i]}`
-        );
-        let json = await res.json();
-        console.log(json, nameArray[i]);
-        url += `+${json.idGroup.rxnormId[0]}`;
-      }
-      try {
-        let response = await fetch(url);
-        let obj = await response.json();
-        console.log(obj.fullInteractionTypeGroup[0].fullInteractionType);
-        setData(obj.fullInteractionTypeGroup[0].fullInteractionType);
-      } catch (error) {
-        console.log(error);
-        setData([]);
-      }
-    }
-    let arr = fetchData(dummy);
-    console.log('arr', arr);
-    setData(arr);
-  }, []);
-  return <div className="hi">{data.length}</div>;
+  return <div className="hi">{itemInteractions.length}</div>;
 }
 
 export default PrescriptionDisplay;
--- medi-check/src/Hooks/useInteractionsFromName.js
+++ medi-check/src/Hooks/useInteractionsFromName.js
@@ -5,23 +5,23 @@ export default function useInteractions(nameArray) {
   useEffect(() => {
     async function fetchData(nameArray) {
       let url = 'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=';
-      for (let i = 0; nameArray.length; i++) {
+      for (let i = 0; i < nameArray.length; i++) {
         let res = await fetch(
           `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${nameArray[i]}`
         );
         let json = await res.json();
+        console.log(nameArray, json);
         url += `+${json.idGroup.rxnormId[0]}`;
       }
       try {
         let response = await fetch(url);
         let obj = await response.json();
-        return obj.fullIntertactionTypeGroup;
+        setData(obj.fullInteractionTypeGroup[0].fullInteractionType);
       } catch (error) {
         console.log(error);
-        return [];
       }
     }
-    setData(fetchData(nameArray));
+    fetchData(nameArray);
   }, [nameArray]);
   return data;
 }
