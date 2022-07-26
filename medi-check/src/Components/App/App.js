26d5ec087d467964fe1efb6163328d46c082188f
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-07-26 16:14:38 +0100

    Started the search functionality

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,14 +1,15 @@
-
-import './App.css';
-import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
+import "./App.css";
+import PrescriptionDisplay from "../Patient/PrescriptionDisplay";
+import PatientList from "../Doctor/PatientList";
+import SearchBar from "../Doctor/Searchbar+CreatePatient";
 
 function App() {
   return (
     <div className="App">
-      <PrescriptionDisplay></PrescriptionDisplay>
+      <PatientList />
+      <SearchBar />
     </div>
   );
-
 }
 
 export default App;
--- medi-check/src/Components/Doctor/Searchbar+CreatePatient/index.js
+++ medi-check/src/Components/Doctor/Searchbar+CreatePatient/index.js
@@ -1,1 +1,54 @@
-//
+////
+import { useState } from "react";
+import { dummyList } from "../PatientList/Patient";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
+
+  function handleChange(e) {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
+  function searchPatient(e) {
+    e.preventDefault();
+    typeof (search[0] === "number") ? searchByNHSNumber() : searchByName();
+  }
+
+  //function used to call patient data from back end from search
+  function searchByNHSNumber() {
+    for (let i = 0; i < dummyList.length; i++) {
+      if (dummyList[i].nhsNumber === Number(search)) {
+        console.log(dummyList[i]);
+        return dummyList[i];
+      }
+    }
+  }
+
+  //function used to call patient data from back end from search
+  function searchByName() {
+    for (let i = 0; i < dummyList.length; i++) {
+      if (dummyList[i].FirstNames === search) {
+        console.log(dummyList[i]);
+        return dummyList[i];
+      }
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={searchPatient}>
+        <label>
+          Patient Search :
+          <input
+            type="text"
+            placeholder="Enter patient name or NHS number"
+            value={search}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
