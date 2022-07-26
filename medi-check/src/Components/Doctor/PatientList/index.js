3b37bc49346af5e9ff8e73b3d35eb75739b23ac5
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-07-26 09:18:54 +0100

    started PatientList component - mapping through dummy data, name d patientList (with small p)

--- medi-check/src/Components/Doctor/PatientList/index.js
+++ medi-check/src/Components/Doctor/PatientList/index.js
@@ -1,1 +1,16 @@
-//
+import Patient from "./Patient";
+//pass in patientList from Patient list (from db)
+function PatientList({ list }) {
+    return (
+        <div>
+            {list.map((patient) => {
+                <li>{props.firstName}</li>;
+            })}
+        </div>
+    );
+}
+
+export default PatientList;
+//    <PatientList list={patientList} />
+//import PatientList from "../Doctor/PatientList";
+// import patientList from "../Doctor/PatientList/Patient";
