0798a9e3a54b1a6957a00c80a0fbf030a9bcb55c
brycen <brycensoc@gmail.com>
2022-07-26 10:44:55 +0100

    got relevant info out of API, ready to use next component

--- medi-check/src/Components/Patient/PrescriptionDisplay/index.js
+++ medi-check/src/Components/Patient/PrescriptionDisplay/index.js
@@ -5,18 +5,41 @@ import { dummy, testInteractions } from './dummyData';
 function PrescriptionDisplay() {
   // let itemInteractions = useInteractions(dummy);
   let itemInteractions = testInteractions;
-  console.log(itemInteractions);
+  // console.log(itemInteractions, Array.isArray(itemInteractions));
   // console.log("rawr", itemInteractions[0].interactionPair[0].severity)
-  let i = 0
+  let i = 0;
+  let combo = dummy.map((item) => {
+    let overview = itemInteractions.filter((info) => {
+      return (
+        item === info.minConcept[0].name || item === info.minConcept[1].name
+      );
+    });
+    let relevantInfo = overview.map((item) => {
+      return {
+        description: item.interactionPair[0].description,
+        drugs: item.minConcept,
+      };
+    });
+    return {
+      drug: item,
+      interactionInfo: relevantInfo,
+    };
+  });
+  console.log('done?', combo);
   return (
     <>
-  <div className="hi">{itemInteractions.length}</div>
-  <div>Medicine 1: {itemInteractions[i].minConcept[0].name}</div>
-  <div>Medicine 2: {itemInteractions[i].minConcept[1].name}</div>
-  <div>Severity of interaction: {itemInteractions[i].interactionPair[0].severity}</div>
-  <div>Description: {itemInteractions[i].interactionPair[0].description}</div>
-  </>
-  )
+      <div className="hi">{itemInteractions.length}</div>
+      <div>Medicine 1: {itemInteractions[i].minConcept[0].name}</div>
+      <div>Medicine 2: {itemInteractions[i].minConcept[1].name}</div>
+      <div>
+        Severity of interaction:{' '}
+        {itemInteractions[i].interactionPair[0].severity}
+      </div>
+      <div>
+        Description: {itemInteractions[i].interactionPair[0].description}
+      </div>
+    </>
+  );
 }
 
 export default PrescriptionDisplay;
