dc43ae1a4608b805d27e42aca246a2e8ae70b557
brycen <brycenbb1996@gmail.com>
2022-07-27 16:11:51 +0100

    functionality - need to merge searchBar branch

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,17 +1,16 @@
-import PatientNavBar from "../Patient/NavBar";
-import "./App.css";
-import PrescriptionDisplay from "../Patient/PrescriptionDisplay";
-import UserSearchBar from "../Patient/Searchbar";
-import Footer from "../Doctor/Footer";
+import PatientNavBar from '../Patient/NavBar';
+import './App.css';
+import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
+import UserSearchBar from '../Patient/Searchbar';
+import Footer from '../Doctor/Footer';
+import { DoctorHome } from '../Doctor/DoctorHome';
 function App() {
-    return (
-        <div className="App">
-            <PatientNavBar />
-            <UserSearchBar />
-            <PrescriptionDisplay />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <DoctorHome></DoctorHome>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- medi-check/src/Components/Doctor/PatientList/index.js
+++ medi-check/src/Components/Doctor/PatientList/index.js
@@ -1,26 +1,38 @@
 //pass in patientList from Patient list (from db)
+import { useState } from 'react';
 import './index.css';
 import { Patient } from './Patient';
 
 function PatientList({ list }) {
-  return (
-    <div>
-      <ul className="mappedPatient">
-        {list.map((patient) => {
-          return (
-            <Patient
-              className="individualPatient"
-              key={patient.nhsNumber}
-              title={patient.Title}
-              firstname={patient.FirstNames}
-              surname={patient.Surname}
-              nhs={patient.nhsNumber}
-            ></Patient>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  const [patient, setPatient] = useState(null);
+  console.log('patient', patient);
+  function resetPatient() {
+    setPatient(null);
+  }
+  if (patient !== null) {
+    return <div onClick={resetPatient}> {patient}</div>;
+  } else {
+    return (
+      <div>
+        <ul className="mappedPatient">
+          {list.map((patient, index) => {
+            return (
+              <Patient
+                index={index}
+                setPatient={setPatient}
+                className="individualPatient"
+                key={patient.nhsNumber}
+                title={patient.Title}
+                firstname={patient.FirstNames}
+                surname={patient.Surname}
+                nhs={patient.nhsNumber}
+              ></Patient>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default PatientList;
