978990595a98704c3401dc45743df6ccb766dc90
brycen <brycenbb1996@gmail.com>
2022-07-27 15:23:58 +0100

    sort works , allows for sort in both directions

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,15 +1,12 @@
-
 import PatientNavBar from '../Patient/NavBar';
 import './App.css';
 import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
 import UserSearchBar from '../Patient/Searchbar';
+import DoctorHome from '../Doctor/DoctorHome';
 function App() {
   return (
     <div className="App">
-      <PatientNavBar />
-      <UserSearchBar></UserSearchBar>
-      <PrescriptionDisplay></PrescriptionDisplay>
-
+      <DoctorHome />
     </div>
   );
 }
