b14aa406b888c45b103842cb6a36da4c8c2c7b3d
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-07-27 14:03:57 +0100

    remembered to save after updating app.js

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import PatientNavBar from "../Patient/NavBar";
 import "./App.css";
 import PrescriptionDisplay from "../Patient/PrescriptionDisplay";
@@ -11,22 +10,6 @@ function App() {
             <PrescriptionDisplay />
         </div>
     );
-=======
-
-import PatientNavBar from '../Patient/NavBar';
-import './App.css';
-import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
-import UserSearchBar from '../Patient/Searchbar';
-function App() {
-  return (
-    <div className="App">
-      <PatientNavBar />
-      <UserSearchBar></UserSearchBar>
-      <PrescriptionDisplay></PrescriptionDisplay>
-
-    </div>
-  );
->>>>>>> 108fb25a82b268a607861844cf6e382c6ab13420
 }
 
 export default App;
