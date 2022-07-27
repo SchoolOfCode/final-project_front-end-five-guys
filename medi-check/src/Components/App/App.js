26c722b133fac1a44aaee21746f276a35e2c8aa0
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-07-27 14:09:31 +0100

    footer rendered

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -2,12 +2,14 @@ import PatientNavBar from "../Patient/NavBar";
 import "./App.css";
 import PrescriptionDisplay from "../Patient/PrescriptionDisplay";
 import UserSearchBar from "../Patient/Searchbar";
+import Footer from "../Doctor/Footer";
 function App() {
     return (
         <div className="App">
             <PatientNavBar />
             <UserSearchBar />
             <PrescriptionDisplay />
+            <Footer />
         </div>
     );
 }
