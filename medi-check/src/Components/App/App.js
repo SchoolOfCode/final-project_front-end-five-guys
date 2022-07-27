c73a6962e6b43c369943980f4935a5c3f86b9c8f
TomBirbeck <tombirbeck47@gmail.com>
2022-07-27 09:13:57 +0100

    basic patient navbar set up

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,10 +1,11 @@
-
+import PatientNavBar from '../Patient/NavBar';
 import './App.css';
 import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
 
 function App() {
   return (
     <div className="App">
+      <PatientNavBar/>
       <PrescriptionDisplay></PrescriptionDisplay>
     </div>
   );
