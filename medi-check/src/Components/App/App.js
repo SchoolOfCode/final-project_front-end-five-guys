0bf079903a13be0ac20a1241cc0ed3c41f959ccf
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-07-27 14:01:24 +0100

    self-closed components

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,15 +1,15 @@
-import PatientNavBar from '../Patient/NavBar';
-import './App.css';
-import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
-import UserSearchBar from '../Patient/Searchbar';
+import PatientNavBar from "../Patient/NavBar";
+import "./App.css";
+import PrescriptionDisplay from "../Patient/PrescriptionDisplay";
+import UserSearchBar from "../Patient/Searchbar";
 function App() {
-  return (
-    <div className="App">
-      <PatientNavBar />
-      <UserSearchBar></UserSearchBar>
-      <PrescriptionDisplay></PrescriptionDisplay>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PatientNavBar />
+            <UserSearchBar />
+            <PrescriptionDisplay />
+        </div>
+    );
 }
 
 export default App;
