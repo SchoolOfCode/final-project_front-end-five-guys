b354522f70480808116356f10fcbff52f1dc5d1a
TomBirbeck <tombirbeck47@gmail.com>
2022-07-28 10:31:28 +0100

    set up basic modal, working on functionality

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-
+import { NewUserPrompt } from '../Patient/NewUserPrompt';
 
 
 import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
 import UserSearchBar from '../Patient/Searchbar';
 import Footer from '../Doctor/Footer';
-import { DoctorHome } from '../Doctor/DoctorHome';
+import  DoctorHome  from '../Doctor/DoctorHome';
 function App() {
   return (
     <div className="App">
       <DoctorHome></DoctorHome>
-      <Footer />
+      <NewUserPrompt />
     </div>
   );
 }
--- medi-check/src/Components/Patient/NewUserPrompt/index.js
+++ medi-check/src/Components/Patient/NewUserPrompt/index.js
@@ -1,1 +1,48 @@
-//
+import {useState} from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export function NewUserPrompt({email}) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [id, setID] = useState("");
+
+  function handleChange(e){
+    setID(e.target.value);
+  }
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Hello. As you're a new user, please input the security ID given to you by your GP below:
+          </Typography>
+          <input type = "text" onChange = {handleChange} value = {id}></input>
+          <button>Submit</button>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
