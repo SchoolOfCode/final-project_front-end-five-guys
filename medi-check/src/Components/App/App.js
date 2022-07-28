91ff2b752a96bcaa1d671f16acba3f0add1225a7
TomBirbeck <tombirbeck47@gmail.com>
2022-07-28 14:15:09 +0100

    setting upstream for diary

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -1,16 +1,16 @@
-import './App.css';
+import "./App.css";
+import { DiaryModal } from "../Patient/Diary";
 
-
-
-import PrescriptionDisplay from '../Patient/PrescriptionDisplay';
-import UserSearchBar from '../Patient/Searchbar';
-import Footer from '../Doctor/Footer';
-import { DoctorHome } from '../Doctor/DoctorHome';
+import PrescriptionDisplay from "../Patient/PrescriptionDisplay";
+import UserSearchBar from "../Patient/Searchbar";
+import Footer from "../Doctor/Footer";
+import { DoctorHome } from "../Doctor/DoctorHome";
 function App() {
   return (
     <div className="App">
       <DoctorHome></DoctorHome>
       <Footer />
+      <DiaryModal />
     </div>
   );
 }
---
+++ medi-check/src/Components/Patient/Diary/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export function DiaryModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Diary</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h2" component="h2">
+            Diary
+          </Typography>
+          <textarea></textarea>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
