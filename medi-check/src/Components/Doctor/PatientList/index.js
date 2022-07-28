b65a1ce3b93a497d3bd1af72ee5f10f3b2c61650
brycen <brycensoc@gmail.com>
2022-07-28 10:12:24 +0100

    reset takes you out of patient file

--- medi-check/src/Components/Doctor/PatientList/index.js
+++ medi-check/src/Components/Doctor/PatientList/index.js
@@ -1,95 +1,90 @@
 //pass in patientList from Patient list (from db)
-import { useState } from "react";
-import PatientFile from "../PatientFile";
-import "./index.css";
-import { Patient } from "./Patient";
+import { useState } from 'react';
+import PatientFile from '../PatientFile';
+import './index.css';
+import { Patient } from './Patient';
 
-function PatientList({ list }) {
-    const [filter, setFilter] = useState({ type: "", reverse: false });
-    const [patient, setPatient] = useState(null);
-    console.log("patient", patient);
-    function resetPatient() {
-        setPatient(null);
-    }
-    function compare(a, b) {
-        // console.log(a, b);
-        if (filter.type === "first") {
-            if (a.FirstNames.toUpperCase() > b.FirstNames.toUpperCase()) {
-                return filter.reverse ? -1 : 1;
-            } else if (
-                a.FirstNames.toUpperCase() < b.FirstNames.toUpperCase()
-            ) {
-                return filter.reverse ? 1 : -1;
-            }
-            return 0;
-        } else if (filter.type === "last") {
-            if (a.Surname.toUpperCase() > b.Surname.toUpperCase()) {
-                return filter.reverse ? -1 : 1;
-            } else if (a.Surname.toUpperCase() < b.Surname.toUpperCase()) {
-                return filter.reverse ? 1 : -1;
-            }
-            return 0;
-        } else {
-            if (a.nhsNumber > b.nhsNumber) {
-                return filter.reverse ? -1 : 1;
-            } else if (a.nhsNumber < b.nhsNumber) {
-                return filter.reverse ? 1 : -1;
-            }
-            return 0;
-        }
-    }
-    function handleSort(sortBy) {
-        setFilter({
-            type: sortBy,
-            reverse: sortBy === filter.type ? !filter.reverse : false,
-        });
-    }
-    if (patient !== null) {
-        return (
-            <PatientFile info={patient} onClick={resetPatient}></PatientFile>
-        );
+function PatientList({ list, patient, setPatient }) {
+  const [filter, setFilter] = useState({ type: '', reverse: false });
+  console.log('patient', patient);
+  function resetPatient() {
+    setPatient(null);
+  }
+  function compare(a, b) {
+    // console.log(a, b);
+    if (filter.type === 'first') {
+      if (a.FirstNames.toUpperCase() > b.FirstNames.toUpperCase()) {
+        return filter.reverse ? -1 : 1;
+      } else if (a.FirstNames.toUpperCase() < b.FirstNames.toUpperCase()) {
+        return filter.reverse ? 1 : -1;
+      }
+      return 0;
+    } else if (filter.type === 'last') {
+      if (a.Surname.toUpperCase() > b.Surname.toUpperCase()) {
+        return filter.reverse ? -1 : 1;
+      } else if (a.Surname.toUpperCase() < b.Surname.toUpperCase()) {
+        return filter.reverse ? 1 : -1;
+      }
+      return 0;
     } else {
-        return (
-            <div>
-                <div>
-                    <div
-                        onClick={() => {
-                            handleSort("first");
-                        }}
-                    >
-                        First name
-                    </div>
-                    <div
-                        onClick={() => {
-                            handleSort("last");
-                        }}
-                    >
-                        Surname
-                    </div>
-                    <div
-                        onClick={() => {
-                            handleSort("number");
-                        }}
-                    >
-                        NHS Number
-                    </div>
-                </div>
-                <ul className="mappedPatient">
-                    {list.sort(compare).map((patient, index) => {
-                        console.log("patient-detials", patient);
-                        return (
-                            <Patient
-                                index={index}
-                                setPatient={setPatient}
-                                className="individualPatient"
-                                key={patient.nhsNumber}
-                                patientInfo={patient}
-                            ></Patient>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
+      if (a.nhsNumber > b.nhsNumber) {
+        return filter.reverse ? -1 : 1;
+      } else if (a.nhsNumber < b.nhsNumber) {
+        return filter.reverse ? 1 : -1;
+      }
+      return 0;
     }
+  }
+  function handleSort(sortBy) {
+    setFilter({
+      type: sortBy,
+      reverse: sortBy === filter.type ? !filter.reverse : false,
+    });
+  }
+  if (patient !== null) {
+    return <PatientFile info={patient} onClick={resetPatient}></PatientFile>;
+  } else {
+    return (
+      <div>
+        <div>
+          <div
+            onClick={() => {
+              handleSort('first');
+            }}
+          >
+            First name
+          </div>
+          <div
+            onClick={() => {
+              handleSort('last');
+            }}
+          >
+            Surname
+          </div>
+          <div
+            onClick={() => {
+              handleSort('number');
+            }}
+          >
+            NHS Number
+          </div>
+        </div>
+        <ul className="mappedPatient">
+          {list.sort(compare).map((patient, index) => {
+            console.log('patient-detials', patient);
+            return (
+              <Patient
+                index={index}
+                setPatient={setPatient}
+                className="individualPatient"
+                key={patient.nhsNumber}
+                patientInfo={patient}
+              ></Patient>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 export default PatientList;
