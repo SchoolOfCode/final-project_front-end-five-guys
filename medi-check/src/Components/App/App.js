1284363393f31a11ac09ef0b5feb330a1f2dece0
TomBirbeck <tombirbeck47@gmail.com>
2022-07-29 15:22:24 +0100

    added buttons on home page to select from doctor/patient

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -3,14 +3,29 @@ import "./App.css";
 import { PatientHome } from "../Patient/PatientHome";
 import Footer from "../Doctor/Footer";
 import DoctorHome from "../Doctor/DoctorHome";
+import { useState } from "react";
 
 function App() {
+  const [display, setDisplay] = useState("");
+
+  function handlePatientClick() {
+    setDisplay("patient");
+  }
+  function handleDoctorClick() {
+    setDisplay("doctor");
+  }
+
   return (
     <div className="App">
-      <DoctorHome></DoctorHome>
-      <PatientHome />
-
-      <Footer />
+      <button onClick={handlePatientClick}>Patient</button>
+      <button onClick={handleDoctorClick}> Doctor</button>
+      {display === "patient" ? (
+        <PatientHome />
+      ) : display === "doctor" ? (
+        <DoctorHome />
+      ) : (
+        <p>Please select a home page to display: </p>
+      )}
     </div>
   );
 }
