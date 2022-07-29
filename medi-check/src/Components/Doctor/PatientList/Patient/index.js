1da1ae79323ccec035465c214ef1ea3f568a5b78
brycen <brycensoc@gmail.com>
2022-07-29 11:28:34 +0100

    changed going into patient file to require double click instead of single

--- medi-check/src/Components/Doctor/PatientList/Patient/index.js
+++ medi-check/src/Components/Doctor/PatientList/Patient/index.js
@@ -2,77 +2,76 @@
 //dummy data for front end
 
 export const dummyList = [
-    {
-        Title: "Mr",
-        FirstNames: "Roger",
-        Surname: "Smith",
-        dob: 24012002,
-        gender: "Male",
-        ethnicity: "Asian",
-        address: "122 Street Road",
-        postcode: "b1 2qt",
-        phoneNumber: 12101234560,
-        allergies: ["paracetamol", "diazepam"],
-        nhsNumber: 1234567891,
-        gpSurgery: "Hall Green Health",
-    },
-    {
-        Title: "Miss",
-        FirstNames: "Victoria",
-        Surname: "Smooth",
-        dob: 27092000,
-        gender: "Female",
-        ethnicity: "Caucasian",
-        address: "122 Street Road",
-        postcode: "b1 2qt",
-        phoneNumber: 12101234560,
-        allergies: ["none"],
-        nhsNumber: 1234567892,
-        gpSurgery: "Hall Green Health",
-    },
-    {
-        Title: "Mrs",
-        FirstNames: "Sam",
-        Surname: "Jam",
-        dob: 20032005,
-        gender: "Female",
-        ethnicity: "Caucasian",
-        address: "129 Street Road",
-        postcode: "b1 2rt",
-        phoneNumber: 12101944560,
-        allergies: [],
-        nhsNumber: 1234987891,
-        gpSurgery: "Hall Green Health",
-    },
-    {
-        Title: "Miss",
-        FirstNames: "Katie",
-        Surname: "Lefoe",
-        dob: 12022005,
-        gender: "Female",
-        ethnicity: "African",
-        address: "122 Lane Road",
-        postcode: "b7 4es",
-        phoneNumber: 31401234560,
-        allergies: ["paracetamol", "diazepam"],
-        nhsNumber: 4784564222,
-        gpSurgery: "Grove Blue Health",
-    },
+  {
+    Title: 'Mr',
+    FirstNames: 'Roger',
+    Surname: 'Smith',
+    dob: 24012002,
+    gender: 'Male',
+    ethnicity: 'Asian',
+    address: '122 Street Road',
+    postcode: 'b1 2qt',
+    phoneNumber: 12101234560,
+    allergies: ['paracetamol', 'diazepam'],
+    nhsNumber: 1234567891,
+    gpSurgery: 'Hall Green Health',
+  },
+  {
+    Title: 'Miss',
+    FirstNames: 'Victoria',
+    Surname: 'Smooth',
+    dob: 27092000,
+    gender: 'Female',
+    ethnicity: 'Caucasian',
+    address: '122 Street Road',
+    postcode: 'b1 2qt',
+    phoneNumber: 12101234560,
+    allergies: ['none'],
+    nhsNumber: 1234567892,
+    gpSurgery: 'Hall Green Health',
+  },
+  {
+    Title: 'Mrs',
+    FirstNames: 'Sam',
+    Surname: 'Jam',
+    dob: 20032005,
+    gender: 'Female',
+    ethnicity: 'Caucasian',
+    address: '129 Street Road',
+    postcode: 'b1 2rt',
+    phoneNumber: 12101944560,
+    allergies: [],
+    nhsNumber: 1234987891,
+    gpSurgery: 'Hall Green Health',
+  },
+  {
+    Title: 'Miss',
+    FirstNames: 'Katie',
+    Surname: 'Lefoe',
+    dob: 12022005,
+    gender: 'Female',
+    ethnicity: 'African',
+    address: '122 Lane Road',
+    postcode: 'b7 4es',
+    phoneNumber: 31401234560,
+    allergies: ['paracetamol', 'diazepam'],
+    nhsNumber: 4784564222,
+    gpSurgery: 'Grove Blue Health',
+  },
 ];
 
 //fn component with props, parent lives in PatientList/index.js, changes key properties to be more semantic
 export function Patient(props) {
-    function handleClick() {
-        props.setPatient({ ...props.patientInfo });
-    }
-    return (
-        // a link might need to change in order for it to work
-        // <a href="localhost:3000/users/{patient.id}">
-        <li onClick={handleClick}>
-            {props.patientInfo.Title} {props.patientInfo.FirstNames}{" "}
-            {props.patientInfo.Surname} NHS Number:{" "}
-            {props.patientInfo.nhsNumber}
-        </li>
-        // </a>
-    );
+  function handleClick() {
+    props.setPatient({ ...props.patientInfo });
+  }
+  return (
+    // a link might need to change in order for it to work
+    // <a href="localhost:3000/users/{patient.id}">
+    <li onDoubleClick={handleClick}>
+      {props.patientInfo.Title} {props.patientInfo.FirstNames}{' '}
+      {props.patientInfo.Surname} NHS Number: {props.patientInfo.nhsNumber}
+    </li>
+    // </a>
+  );
 }
