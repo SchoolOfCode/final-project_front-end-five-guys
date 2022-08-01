bd24171dfff183f1c9eef8f2d3e4130c4267d67c
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-08-01 15:14:31 +0100

    fixed hopefully

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -6,39 +6,28 @@ import DoctorHome from "../Doctor/DoctorHome";
 import { useState } from "react";
 
 function App() {
-<<<<<<< HEAD
+    const [display, setDisplay] = useState("");
+
+    function handlePatientClick() {
+        setDisplay("patient");
+    }
+    function handleDoctorClick() {
+        setDisplay("doctor");
+    }
+
     return (
         <div className="App">
-            <DoctorHome></DoctorHome>
-            <PatientHome />
-
-            <Footer />
+            <button onClick={handlePatientClick}>Patient</button>
+            <button onClick={handleDoctorClick}> Doctor</button>
+            {display === "patient" ? (
+                <PatientHome />
+            ) : display === "doctor" ? (
+                <DoctorHome />
+            ) : (
+                <p>Please select a home page to display: </p>
+            )}
         </div>
     );
-=======
-  const [display, setDisplay] = useState("");
-
-  function handlePatientClick() {
-    setDisplay("patient");
-  }
-  function handleDoctorClick() {
-    setDisplay("doctor");
-  }
-
-  return (
-    <div className="App">
-      <button onClick={handlePatientClick}>Patient</button>
-      <button onClick={handleDoctorClick}> Doctor</button>
-      {display === "patient" ? (
-        <PatientHome />
-      ) : display === "doctor" ? (
-        <DoctorHome />
-      ) : (
-        <p>Please select a home page to display: </p>
-      )}
-    </div>
-  );
->>>>>>> 1284363393f31a11ac09ef0b5feb330a1f2dece0
 }
 
 export default App;
