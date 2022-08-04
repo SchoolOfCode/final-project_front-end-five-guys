8d3e7c6ac3ceb209b1c72335165f3251695df00c
TomBirbeck <tombirbeck47@gmail.com>
2022-08-04 14:48:30 +0100

    accessibility menu, pop out working, need to add options and style

--- medi-check/src/Components/Patient/Accessibility/index.js
+++ medi-check/src/Components/Patient/Accessibility/index.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import "./index.css";
 
-export function Accessibility({ handleClick }) {
+export default function Accessibility() {
+  const [open, setOpen] = useState(false);
+  const [menu, setMenu] = useState();
+
+  function handleOpen() {
+    accessRender();
+    open === false ? setOpen(true) : setOpen(false);
+  }
+
+  function accessRender() {
+    !open ? setMenu(<div style={{ height: "200px" }}>HEY</div>) : setMenu();
+  }
+
   return (
     // <button className="accessLogo-button" onClick={handleClick}>
-
-    <img
-      onClick={handleClick}
-      className="accessLogo"
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Accessibility.svg/640px-Accessibility.svg.png"
-      alt="accessibilty logo"
-    />
+    <div>
+      <img
+        onClick={handleOpen}
+        className="accessLogo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Accessibility.svg/640px-Accessibility.svg.png"
+        alt="accessibilty logo"
+      />
+      {menu}
+    </div>
     // </button>
   );
 }
+
+//set a state for open and close menu
+//handleclick to toggle state
+//accessrender returns div
+//inside accessrender dark mode toggle, font size toggle and colourblin radio buttons
