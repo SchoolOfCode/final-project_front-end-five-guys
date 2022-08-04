1b8a033b24b0182afef759a48014b75f94cbd81d
brycen <brycensoc@gmail.com>
2022-08-04 14:28:53 +0100

    results for getting patient according to DOCTOR_EMAIL works

--- medi-check/src/Components/Doctor/DoctorHome/index.js
+++ medi-check/src/Components/Doctor/DoctorHome/index.js
@@ -1,69 +1,75 @@
-import PatientList from "../PatientList";
-import SearchBar from "../Searchbar+CreatePatient";
-import { useEffect, useState } from "react";
-import { dummyList } from "../PatientList/Patient";
-import Footer from "../Footer";
-import CreatePatientDialog from "../../../MUIcomponents/CreatePatientModal";
-import "./DoctorHome.css";
-import Header from "../DoctorHeader";
-import FadeMenu from "../../../MUIcomponents/AccessibilityMenu";
+import PatientList from '../PatientList';
+import SearchBar from '../Searchbar+CreatePatient';
+import { useEffect, useState } from 'react';
+import { dummyList } from '../PatientList/Patient';
+import Footer from '../Footer';
+import CreatePatientDialog from '../../../MUIcomponents/CreatePatientModal';
+import './DoctorHome.css';
+import Header from '../DoctorHeader';
+import FadeMenu from '../../../MUIcomponents/AccessibilityMenu';
 
 //If running into problems based on searching consequitively, can use other idea of passing search term into patientlist and letting filter happen there.
 function DoctorHome() {
-    const [list, setList] = useState([]);
-    const [filteredList, setFilteredList] = useState([]);
-    const [patient, setPatient] = useState(null);
+  const [list, setList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [patient, setPatient] = useState(null);
 
-    //used for reset button to trigger fresh patient list recall
-    const [reset, setReset] = useState(0);
+  //used for reset button to trigger fresh patient list recall
+  const [reset, setReset] = useState(0);
+  const DOCTOR_EMAIL = 'bens@gmail.com';
 
-    //useEffect to be filled with doctor owned user/patients via doctors_id
-    useEffect(() => {
-        // pull info by doctors id
-        setList([...dummyList]); //this is imported for proof of concept
-    }, [reset]);
+  useEffect(() => {
+    async function getPatients() {
+      let res = await fetch(
+        `http://localhost:3001/patients?doctoremail=${DOCTOR_EMAIL}`
+      );
+      console.log(res);
+    }
+    getPatients();
+  }, []);
+  // //useEffect to be filled with doctor owned user/patients via doctors_id
+  // useEffect(() => {
+  //     // pull info by doctors id
+  //     setList([...dummyList]); //this is imported for proof of concept
+  // }, [reset]);
 
-    return (
-        <div className="doctor-home">
-            <Header name="Smuggles" />
-            <section className="doctor-navigation">
-                <div className="searchbar">
-                    <SearchBar
-                        list={list}
-                        setFilteredList={setFilteredList}
-                        setList={setList}
-                        setReset={setReset}
-                        reset={reset}
-                        setPatient={setPatient}
-                    />
-                </div>
-
-                <CreatePatientDialog />
-            </section>
-            <div className="patient-table-con">
-                {filteredList.length === 0 ? (
-                    <PatientList
-                        list={list}
-                        patient={patient}
-                        setPatient={setPatient}
-                    />
-                ) : (
-                    <PatientList
-                        list={filteredList}
-                        patient={patient}
-                        setPatient={setPatient}
-                    />
-                )}
-            </div>
-
-            {/* <Accessibility /> */}
-            <FadeMenu />
-
-            <Footer />
-            {/* {!filteredList && <PatientList list={list} />} */}
-            {/* <PatientList list={list} /> */}
+  return (
+    <div className="doctor-home">
+      <Header name="Smuggles" />
+      <section className="doctor-navigation">
+        <div className="searchbar">
+          <SearchBar
+            list={list}
+            setFilteredList={setFilteredList}
+            setList={setList}
+            setReset={setReset}
+            reset={reset}
+            setPatient={setPatient}
+          />
         </div>
-    );
+
+        <CreatePatientDialog />
+      </section>
+      <div className="patient-table-con">
+        {filteredList.length === 0 ? (
+          <PatientList list={list} patient={patient} setPatient={setPatient} />
+        ) : (
+          <PatientList
+            list={filteredList}
+            patient={patient}
+            setPatient={setPatient}
+          />
+        )}
+      </div>
+
+      {/* <Accessibility /> */}
+      <FadeMenu />
+
+      <Footer />
+      {/* {!filteredList && <PatientList list={list} />} */}
+      {/* <PatientList list={list} /> */}
+    </div>
+  );
 }
 
 export default DoctorHome;
