b3e1866874ca5b92d7ba1c7ab570d1910e87d421
Rajesh-Reel <rajreel2@hotmail.com>
2022-08-05 15:46:20 +0100

    added css variables to default color and dark mode colors!

--- medi-check/src/Components/Doctor/DoctorHome/index.js
+++ medi-check/src/Components/Doctor/DoctorHome/index.js
@@ -1,12 +1,12 @@
-import PatientList from '../PatientList';
-import SearchBar from '../Searchbar+CreatePatient';
-import { useEffect, useState } from 'react';
-import { dummyList } from '../PatientList/Patient';
-import Footer from '../Footer';
-import CreatePatientDialog from '../../../MUIcomponents/CreatePatientModal';
-import './DoctorHome.css';
-import Header from '../DoctorHeader';
-import Accessibility from '../../Patient/Accessibility';
+import PatientList from "../PatientList";
+import SearchBar from "../Searchbar+CreatePatient";
+import { useEffect, useState } from "react";
+// import { dummyList } from '../PatientList/Patient';
+import Footer from "../Footer";
+import CreatePatientDialog from "../../../MUIcomponents/CreatePatientModal";
+import "./DoctorHome.css";
+import Header from "../DoctorHeader";
+import Accessibility from "../../Patient/Accessibility";
 
 //If running into problems based on searching consequitively, can use other idea of passing search term into patientlist and letting filter happen there.
 function DoctorHome({ setDarkMode }) {
@@ -16,7 +16,7 @@ function DoctorHome({ setDarkMode }) {
 
   //used for reset button to trigger fresh patient list recall
   const [reset, setReset] = useState(0);
-  const DOCTOR_EMAIL = 'bens@gmail.com';
+  const DOCTOR_EMAIL = "bens@gmail.com";
 
   useEffect(() => {
     async function getPatients() {
@@ -25,7 +25,7 @@ function DoctorHome({ setDarkMode }) {
       );
       console.log(res);
       let json = await res.json();
-      console.log(';s', json);
+      console.log(";s", json);
       setList([...json.data]);
     }
     getPatients();
@@ -64,7 +64,6 @@ function DoctorHome({ setDarkMode }) {
           />
         )}
       </div>
-
 
       <Accessibility setDarkMode={setDarkMode} />
 
