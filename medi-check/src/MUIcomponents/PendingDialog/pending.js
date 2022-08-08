5c7d4a35125c65cb1d465afc4b991781042fb78f
brycen <brycensoc@gmail.com>
2022-08-08 16:57:41 +0100

    can post and delete to DB, needs to be re-made so that the list can update automatically

--- medi-check/src/MUIcomponents/PendingDialog/pending.js
+++ medi-check/src/MUIcomponents/PendingDialog/pending.js
@@ -24,12 +24,14 @@ export default function PendingDialog({ open, setOpen }) {
     }
     getPending();
   }, []);
+
   useEffect(() => {
     async function updatePending() {
-      if (prescription.approval) {
+      console.log(';asdasda', settledPrescription);
+      if (settledPrescription.approved) {
         //post
         const response = await fetch(
-          `https://fiveguysproject.herokuapp.com/prescription/${prescription.patient_id}`,
+          `https://fiveguysproject.herokuapp.com/prescriptions/${prescription.patient_id}`,
           {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -52,12 +54,13 @@ export default function PendingDialog({ open, setOpen }) {
 
       setSettledPrescription({});
     }
-    if (Object.keys(setSettledPrescription).length !== 0) {
+    if (Object.keys(settledPrescription).length !== 0) {
+      console.log('updating pending duie to click');
       updatePending();
     }
   }, [settledPrescription, prescription]);
 
-  console.log('pers', prescription);
+  // console.log('pers', prescription);
 
   const handleClose = (value) => {
     setOpen(false);
@@ -67,115 +70,87 @@ export default function PendingDialog({ open, setOpen }) {
     setPrescription({ ...item });
   }
   function handleDecision(approval) {
+    console.log('in handle decision');
     setSettledPrescription({ ...prescription, approved: approval });
   }
-  /*
-address: "122 Street Road Birmingham"
-date: "2022-07-04"
-dob: "1988-02-01"
-dosage: "250"
-email: "rsmith123@email.com"
-ethnicity: "Irish"
-firstname: "Roger"
-frequency: "every four hours"
-gender: "Male"
-gpsurgery: "Happy Health"
-measurement: "mg"
-monitoring: false
-monitoringfrequency: "week"
-monitoringschedule: "0"
-name: "ibuprofen"
-nhsnumber: "123456789"
-override: ""
-patient_id: 1
-pending_id: 1
-phonenumber: "01213457854"
-postcode: "B91 7QY"
-pregnant: false
-prepaid: "2022-09-01"
-quantity: "300"
-reason: "headaches"
-status: "active"
-surname: "Smith"
-title: "Mr"
-total: "1000"
-type: "repeat"
-weight: "100"
-*/
 
   return (
     <>
-      <Dialog onClose={handleClose} open={open} maxWidth="lg" fullWidth="true">
-        <div className="pendingBox">
-          <section>
-            {' '}
-            <DialogTitle>Pending Prescriptions</DialogTitle>
-            <>
+      <Dialog onClose={handleClose} open={open} maxWidth="lg" fullWidth>
+        {pending.length === 0 ? (
+          <div>No current pending prescriptions</div>
+        ) : (
+          <div className="pendingBox">
+            <section>
               {' '}
-              {pending.map((item) => {
-                return (
-                  <Typography
-                    onClick={() => {
-                      handleOpen(item);
-                    }}
-                  >
-                    {item.firstname} {item.surname} {item.name}
-                  </Typography>
-                );
-              })}
-            </>
-          </section>
-          <section>
-            <h4>
-              {prescription.firstname} {prescription.surname}{' '}
-              {prescription.date}
-            </h4>
-            <div>
-              {prescription.name} {'-'}
-              {prescription.quantity + ', '}
-              {prescription.dosage}
-              {prescription.measurement}
-              {', '}
-              {prescription.frequency}
-              {', '}
-              {prescription.total + ' total'}
-              {' (' + prescription.type + ')'}
-            </div>
-
-            <div>{prescription.reason}</div>
-            <div>{'Pregnancy status: ' + prescription.pregnant}</div>
-            <div>{'Weight: ' + prescription.weight + 'kg'}</div>
-            <div>{'DOB: ' + prescription.dob}</div>
-            {prescription.monitoring ? (
+              <DialogTitle>Pending Prescriptions</DialogTitle>
+              <>
+                {' '}
+                {pending.map((item) => {
+                  return (
+                    <Typography
+                      onClick={() => {
+                        handleOpen(item);
+                      }}
+                    >
+                      {item.firstname} {item.surname} {item.name}
+                    </Typography>
+                  );
+                })}
+              </>
+            </section>
+            <section>
+              <h4>
+                {prescription.firstname} {prescription.surname}{' '}
+                {prescription.date}
+              </h4>
               <div>
-                {'Monitoring every ' +
-                  prescription.monitoringschedule +
-                  ' ' +
-                  prescription.monitoringfrequency}
+                {prescription.name} {'-'}
+                {prescription.quantity + ', '}
+                {prescription.dosage}
+                {prescription.measurement}
+                {', '}
+                {prescription.frequency}
+                {', '}
+                {prescription.total + ' total'}
+                {' (' + prescription.type + ')'}
               </div>
-            ) : (
-              <></>
-            )}
-            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-              <button
-                onClick={() => {
-                  handleDecision(true);
-                }}
-              >
-                {' '}
-                Approve
-              </button>
-              <button
-                onClick={() => {
-                  handleDecision(false);
-                }}
-              >
-                {' '}
-                Deny
-              </button>
-            </div>
-          </section>
-        </div>
+
+              <div>{prescription.reason}</div>
+              <div>{'Pregnancy status: ' + prescription.pregnant}</div>
+              <div>{'Weight: ' + prescription.weight + 'kg'}</div>
+              <div>{'DOB: ' + prescription.dob}</div>
+              {prescription.monitoring ? (
+                <div>
+                  {'Monitoring every ' +
+                    prescription.monitoringschedule +
+                    ' ' +
+                    prescription.monitoringfrequency}
+                </div>
+              ) : (
+                <></>
+              )}
+              <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <button
+                  onClick={() => {
+                    handleDecision(true);
+                  }}
+                >
+                  {' '}
+                  Approve
+                </button>
+                <button
+                  onClick={() => {
+                    handleDecision(false);
+                  }}
+                >
+                  {' '}
+                  Deny
+                </button>
+              </div>
+            </section>
+          </div>
+        )}
       </Dialog>
     </>
   );
