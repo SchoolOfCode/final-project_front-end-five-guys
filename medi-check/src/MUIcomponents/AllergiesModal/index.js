cfd9f5fe3c160850dc79d2026f92854327acc5dc
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-08-09 17:14:34 +0100

    styling changes for allergies modal

--- medi-check/src/MUIcomponents/AllergiesModal/index.js
+++ medi-check/src/MUIcomponents/AllergiesModal/index.js
@@ -7,128 +7,135 @@ import Modal from '@mui/material/Modal';
 import { v4 as uuidv4 } from 'uuid';
 
 const style = {
-  display: 'flex',
-  flexDirection: 'column',
-  objectFit: 'contain',
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    objectFit: 'contain',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
 //const Dummyallergies = ["fur", "pollen", "dust", "apixaban"];
 
 export default function AllergiesModal({ setAnchorEl }) {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const [allergy, setAllergy] = useState({
-    name: '',
-    reaction: '',
-  });
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const [allergy, setAllergy] = useState({
+        name: '',
+        reaction: '',
+    });
 
-  function handleClose() {
-    setOpen(false);
-    setAnchorEl(null);
-  }
+    function handleClose() {
+        setOpen(false);
+        setAnchorEl(null);
+    }
 
-  const [CurrentAllergy, setCurrentAllergy] = useState([]);
+    const [CurrentAllergy, setCurrentAllergy] = useState([]);
 
-  //dummy patient email for back end tie up, not needed when auth0 tied up
-  const pEmail = 'rsmith123@email.com';
+    //dummy patient email for back end tie up, not needed when auth0 tied up
+    const pEmail = 'rsmith123@email.com';
 
-  async function getData() {
-    let response = await fetch(
-      `https://fiveguysproject.herokuapp.com/allergy/?email=${pEmail}`
+    async function getData() {
+        let response = await fetch(
+            `https://fiveguysproject.herokuapp.com/allergy/?email=${pEmail}`
+        );
+        let data = await response.json();
+        setCurrentAllergy(data.data);
+        console.log(data.data);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    function handleText(e) {
+        let updatedName = e.target.name;
+        let obj = allergy;
+        obj[updatedName] = e.target.value;
+        setAllergy({ ...obj });
+    }
+
+    //will be updated to submit data to db when created
+    function handleSubmit() {
+        postAllergy();
+        handleClose();
+        setCurrentAllergy([...CurrentAllergy, allergy]);
+    }
+
+    async function postAllergy() {
+        const db_url = `https://fiveguysproject.herokuapp.com/allergy/${pEmail}`;
+        const newPost = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(allergy),
+        };
+        const res = await fetch(db_url, newPost);
+        console.log(res);
+    }
+
+    return (
+        <div>
+            <Button onClick={handleOpen}>Allergies</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby='modal-modal-title'
+                aria-describedby='modal-modal-description'
+            >
+                <Box sx={style}>
+                    <Typography
+                        sx={{ mb: 0.5 }}
+                        id='modal-modal-title'
+                        variant='h5'
+                        component='h2'
+                    >
+                        Allergies
+                    </Typography>
+                    {CurrentAllergy.length > 0 ? (
+                        CurrentAllergy.map((item) => {
+                            return (
+                                <Typography
+                                    key={uuidv4()}
+                                    id='modal-modal-description'
+                                >
+                                    {item.name}
+                                </Typography>
+                            );
+                        })
+                    ) : (
+                        <Typography>
+                            You have no current recorded allergies
+                        </Typography>
+                    )}
+                    <Typography sx={{ mt: 1, mb: 0.4 }}>New Allergy</Typography>
+                    <textarea
+                        style={{ resize: 'none', height: '5vh', width: '100%' }}
+                        onChange={handleText}
+                        name='name'
+                    ></textarea>{' '}
+                    <Typography sx={{ mb: 0.4 }}>
+                        New Allergy Reaction
+                    </Typography>
+                    <textarea
+                        style={{ resize: 'none', height: '5vh', width: '100%' }}
+                        onChange={handleText}
+                        name='reaction'
+                    ></textarea>
+                    <button
+                        style={{ alignSelf: 'center' }}
+                        onClick={handleSubmit}
+                    >
+                        Submit
+                    </button>
+                </Box>
+            </Modal>
+        </div>
     );
-    let data = await response.json();
-    setCurrentAllergy(data.data);
-    console.log(data.data);
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  function handleText(e) {
-    let updatedName = e.target.name;
-    let obj = allergy;
-    obj[updatedName] = e.target.value;
-    setAllergy({ ...obj });
-  }
-
-  //will be updated to submit data to db when created
-  function handleSubmit() {
-    postAllergy();
-    handleClose();
-    setCurrentAllergy([...CurrentAllergy, allergy]);
-  }
-
-  async function postAllergy() {
-    const db_url = `https://fiveguysproject.herokuapp.com/allergy/${pEmail}`;
-    const newPost = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(allergy),
-    };
-    const res = await fetch(db_url, newPost);
-    console.log(res);
-  }
-
-  return (
-    <div>
-      <Button onClick={handleOpen}>Allergies</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
-        <Box sx={style}>
-          <Typography
-            sx={{ mb: 0.5 }}
-            id='modal-modal-title'
-            variant='h5'
-            component='h2'
-          >
-            Allergies
-          </Typography>
-          {CurrentAllergy.length > 0 ? (
-            CurrentAllergy.map((item) => {
-              return (
-                <Typography
-                  key={uuidv4()}
-                  id='modal-modal-description'
-                  sx={{ ml: 4 }}
-                >
-                  {item.name}
-                </Typography>
-              );
-            })
-          ) : (
-            <Typography>You have no current recorded allergies</Typography>
-          )}
-          <Typography sx={{ mt: 1, mb: 0.4 }}>New Allergy</Typography>
-          <textarea
-            style={{ resize: 'none', height: '5vh', width: '100%' }}
-            onChange={handleText}
-            name='name'
-          ></textarea>{' '}
-          <Typography sx={{ mb: 0.4 }}>New Allergy Reaction</Typography>
-          <textarea
-            style={{ resize: 'none', height: '5vh', width: '100%' }}
-            onChange={handleText}
-            name='reaction'
-          ></textarea>
-          <button style={{ alignSelf: 'center' }} onClick={handleSubmit}>
-            Submit
-          </button>
-        </Box>
-      </Modal>
-    </div>
-  );
 }
