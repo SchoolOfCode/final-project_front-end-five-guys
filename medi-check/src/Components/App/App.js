e867058182cfd7561f44dc1573eb40d11616d842
brycen <brycensoc@gmail.com>
2022-08-09 15:51:07 +0100

    log in - out and conditionally showing them works fine

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -4,11 +4,13 @@ import { PatientHome } from '../Patient/PatientHome';
 //import Footer from "../Doctor/Footer";
 import DoctorHome from '../Doctor/DoctorHome';
 import { useState } from 'react';
-
+import LoginButton from '../../Auth0/login';
+import LogoutButton from '../../Auth0/logout';
+import { useAuth0 } from '@auth0/auth0-react';
 function App() {
   const [display, setDisplay] = useState('');
   const [darkMode, setDarkMode] = useState(false);
-
+  const { user, isAuthenticated, isLoading } = useAuth0();
   function handlePatientClick() {
     setDisplay('patient');
   }
@@ -17,11 +19,14 @@ function App() {
   }
 
   let mode = darkMode ? 'dark-mode App' : 'App';
-
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className={mode}>
       {/* darkMode? <div className="dark-mode"/>:<div className="App"/> */}
-
+      {!user && <LoginButton></LoginButton>}
+      {user && <LogoutButton></LogoutButton>}
       <button onClick={handlePatientClick}>Patient</button>
       <button onClick={handleDoctorClick}> Doctor</button>
       {display === 'patient' ? (
--- medi-check/src/index.js
+++ medi-check/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App.js';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT}
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
