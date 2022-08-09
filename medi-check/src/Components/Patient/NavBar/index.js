aac4cc6d2959fa3ef24732fc16d895272d471242
michaelrushton-dev <michaelwjrushton@gmail.com>
2022-08-09 14:31:51 +0100

    resized diary modal, set dark mode var for px font

--- medi-check/src/Components/Patient/NavBar/index.js
+++ medi-check/src/Components/Patient/NavBar/index.js
@@ -5,14 +5,15 @@ import AllergiesModal from '../../../MUIcomponents/AllergiesModal';
 import Accessibility from '../../Patient/Accessibility';
 
 export default function PatientNavBar({ setDarkMode }) {
-  return (
-    <div className='navbar'>
-      <Accessibility
-        setDarkMode={setDarkMode}
-        Displayclass={'accessLogo-patient'}
-      />
-      <AllergiesModal />
-      <DiaryModal />
-    </div>
-  );
+    return (
+        <div className='navbar'>
+            <Accessibility
+                setDarkMode={setDarkMode}
+                Displayclass={'accessLogo-patient'}
+            />
+
+            <AllergiesModal />
+            <DiaryModal />
+        </div>
+    );
 }
