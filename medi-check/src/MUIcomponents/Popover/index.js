4cefdaa997995352ba0696f1648ead4206b2ef36
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-08-09 15:41:10 +0100

    Amended the notifications style -aligned the number of notification to the centre of the circle -changed the margins in the popover -changed the font size in the popover

--- medi-check/src/MUIcomponents/Popover/index.js
+++ medi-check/src/MUIcomponents/Popover/index.js
@@ -9,82 +9,72 @@ import { TbBellRinging2 } from 'react-icons/tb';
 //
 
 export default function BasicPopover({ data, notifications, prepaid }) {
-    const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
-    //function to return slightly different message for single alerts and multiple alerts
-    function caption() {
-        let caption = '';
-        if (data.length === 1) {
-            caption = 'You need to renew your prescription for:';
-        } else if (data.length > 1) {
-            caption = 'You need to renew your prescriptions for:';
-        } else if (data.length === 0 && prepaid === '') {
-            caption = 'You have no notifications';
-        }
-        return caption;
+  //function to return slightly different message for single alerts and multiple alerts
+  function caption() {
+    let caption = '';
+    if (data.length === 1) {
+      caption = 'You need to renew your prescription for:';
+    } else if (data.length > 1) {
+      caption = 'You need to renew your prescriptions for:';
+    } else if (data.length === 0 && prepaid === '') {
+      caption = 'You have no notifications';
     }
+    return caption;
+  }
 
-    return (
-        <div>
-            <div className="notifications">
-                <TbBellRinging2
-                    className="bell-icon"
-                    sx={{ position: 'relative' }}
-                    aria-describedby={id}
-                    variant="contained"
-                    onClick={handleClick}
-                >
-                    {/* <span>
-                        <TbBellRinging2 />
-                    </span> */}
-                </TbBellRinging2>
-                <div className="notification-number">{notifications}</div>
-            </div>
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-            >
-                <Typography
-                    id="modal-modal-title"
-                    variant="h6"
-                    component="h2"
-                    sx={{ mt: 1, ml: 1 }}
-                >
-                    {prepaid}
-                </Typography>
-                <Typography
-                    id="modal-modal-title"
-                    variant="h6"
-                    component="h2"
-                    sx={{ mt: 1, ml: 1 }}
-                >
-                    {caption()}
-                </Typography>
-                {data.map((item) => {
-                    return (
-                        <Typography key={uuidv4()} sx={{ p: 1, ml: 4 }}>
-                            {item}
-                        </Typography>
-                    );
-                })}
-            </Popover>
-        </div>
-    );
+  return (
+    <div>
+      <div className='notifications'>
+        <TbBellRinging2
+          className='bell-icon'
+          sx={{ position: 'relative' }}
+          aria-describedby={id}
+          variant='contained'
+          onClick={handleClick}
+        ></TbBellRinging2>
+        <div className='notification-number'>{notifications}</div>
+      </div>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Typography className='modal-modal-title' component='h2' sx={{ m: 2 }}>
+          {prepaid}
+        </Typography>
+        <Typography
+          className='modal-modal-title'
+          component='h2'
+          sx={{ mt: 2, mr: 2, ml: 2 }}
+        >
+          {caption()}
+        </Typography>
+        {data.map((item) => {
+          return (
+            <Typography key={uuidv4()} sx={{ ml: 4, p: 0.5 }}>
+              {item}
+            </Typography>
+          );
+        })}
+      </Popover>
+    </div>
+  );
 }
