30847b0e1dd93fccdf78a5b3b56c527c69eeb8b5
brycen <brycensoc@gmail.com>
2022-08-09 16:11:10 +0100

    auth0 done up to point of prompting user to give info to register

--- medi-check/src/Components/App/App.js
+++ medi-check/src/Components/App/App.js
@@ -3,14 +3,41 @@ import './App.css';
 import { PatientHome } from '../Patient/PatientHome';
 //import Footer from "../Doctor/Footer";
 import DoctorHome from '../Doctor/DoctorHome';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoginButton from '../../Auth0/login';
 import LogoutButton from '../../Auth0/logout';
 import { useAuth0 } from '@auth0/auth0-react';
 function App() {
   const [display, setDisplay] = useState('');
   const [darkMode, setDarkMode] = useState(false);
+  const [status, setStatus] = useState(false);
   const { user, isAuthenticated, isLoading } = useAuth0();
+  useEffect(() => {
+    async function findUser() {
+      let doctorCheck = await fetch(
+        `https://fiveguysproject.herokuapp.com/doctor?email=${user.email}`
+      );
+      let doctorData = await doctorCheck.json();
+      if (doctorData.data.length === 0) {
+        let patientCheck = await fetch(
+          `https://fiveguysproject.herokuapp.com/patient?email=${user.email}`
+        );
+        let patientData = await patientCheck.json();
+        if (patientData.data.length === 0) {
+          setStatus(false);
+        } else {
+          setStatus(true);
+        }
+        setDisplay('patient');
+      } else {
+        setDisplay('doctor');
+      }
+      console.log(doctorData);
+    }
+    if (isAuthenticated) {
+      findUser();
+    }
+  }, [isAuthenticated, user]);
   function handlePatientClick() {
     setDisplay('patient');
   }
@@ -30,7 +57,7 @@ function App() {
       <button onClick={handlePatientClick}>Patient</button>
       <button onClick={handleDoctorClick}> Doctor</button>
       {display === 'patient' ? (
-        <PatientHome />
+        <PatientHome registered={status} />
       ) : display === 'doctor' ? (
         <DoctorHome setDarkMode={setDarkMode} />
       ) : (
--- medi-check/src/Components/Patient/PatientHome/index.js
+++ medi-check/src/Components/Patient/PatientHome/index.js
@@ -3,7 +3,13 @@ import PrescriptionDisplay from '../PrescriptionDisplay';
 import { Notifications, dummyData } from '../Notifications';
 import UserSearchBar from '../Searchbar';
 
-export function PatientHome() {
+export function PatientHome({ registered }) {
+  if (!registered) {
+    console.log(
+      'User does not exist in DB, needs to input their code to register'
+    );
+    //Need to prompt the user to put in their code and all that jazz
+  }
   return (
     <>
       <PatientNavBar />
