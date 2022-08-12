81f3e4dca5ad5e059e7951dda1d007ed3bbb7c7e
brycen <brycensoc@gmail.com>
2022-08-12 12:03:19 +0100

    changed pending back

--- medi-check/src/MUIcomponents/PendingDialog/pending.js
+++ medi-check/src/MUIcomponents/PendingDialog/pending.js
@@ -16,7 +16,6 @@ import { useState, useEffect } from 'react';
 import ButtonComponent from '../ButtonComponent';
 import './index.css';
 export default function PendingDialog({ open, setOpen }) {
-
   const [pending, setPending] = useState([]);
   const [prescription, setPrescription] = useState({});
   const [send, setSend] = useState(false);
@@ -119,74 +118,38 @@ export default function PendingDialog({ open, setOpen }) {
                 ? JSON.stringify({ ...prescription, override: reason })
                 : JSON.stringify(prescription),
             }
+          );
+          let json = await response.json();
+          // console.log('posted new prescription!:', json);
         }
-        getPending();
-    }, []);
+        //delete
+        const response2 = await fetch(
+          `https://fiveguysproject.herokuapp.com/pending/${prescription.pending_id}`,
+          {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+          }
+        );
+        let json2 = await response2.json();
+        // console.log('deleted pending prescription:', json2);
 
-    useEffect(() => {
-        async function fetchPrescriptionsAndCheckForInteractions() {
-            if (!settledPrescription.approved) {
-                setSend(true);
-                return;
+        let index;
+        // console.log('pending:', pending);
+        for (let i = 0; i < pending.length; i++) {
+          let keys = Object.keys(pending[i]);
+          console.log('current pending:', pending[i], keys);
+          for (let j = 0; j < keys.length; j++) {
+            if (pending[i][keys[j]] !== settledPrescription[keys[j]]) {
+              break;
             }
-            let prescriptonResponse = await fetch(
-                `https://fiveguysproject.herokuapp.com/prescriptions/${prescription.patient_id}`
-            );
-            let prescriptionJson = await prescriptonResponse.json();
-            let list = prescriptionJson.data;
-            // console.log('asddasadsdasadsdas', list);
-            let url =
-                'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=';
-            for (let i = 0; i < list.length; i++) {
-                let res = await fetch(
-                    `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${list[i].name}`
-                );
-                let json = await res.json();
-                console.log(list[i].name, list);
-                url += `+${json.idGroup.rxnormId[0]}`;
+            if (j === keys.length - 1) {
+              index = i;
             }
-            try {
-                let response = await fetch(url + '&sources=ONCHigh');
-                let obj = await response.json();
-                // console.log('interactions here: ', obj);
-                // console.log('new drug name here: ', prescription.name);
-                if (!obj.fullInteractionTypeGroup) {
-                    setSend(true);
-                    return;
-                }
-                let filtered =
-                    obj.fullInteractionTypeGroup[0].fullInteractionType.filter(
-                        (item) => {
-                            return (
-                                prescription.name.toLowerCase() ===
-                                    item.minConcept[0].name.toLowerCase() ||
-                                prescription.name.toLowerCase() ===
-                                    item.minConcept[1].name.toLowerCase()
-                            );
-                        }
-                    );
-                // console.log('fil', filtered);
-                if (filtered.length !== 0) {
-                    // console.log('There are  interactions with the new drug'); //There is an interaction and we need to stop the closure and display the warning
-                    let temp = [];
-                    for (let i = 0; i < filtered.length; i++) {
-                        temp.push(
-                            filtered[i].minConcept[0].name === prescription.name
-                                ? filtered[i].minConcept[1].name
-                                : filtered[i].minConcept[0].name
-                        );
-                    }
-                    setInteractedDrugs([...temp]);
-                    // console.log('about to set open status');
-                    setOpenStatusC(true);
-                } else {
-                    setSend(true);
-                }
-            } catch (error) {
-                console.log('error', error);
-            }
+          }
+          if (index) {
+            break;
+          }
         }
-
         console.table(
           'Checking progress:',
           index,
@@ -219,65 +182,36 @@ export default function PendingDialog({ open, setOpen }) {
     interactedDrugs.length,
   ]);
 
-                let index;
-                // console.log('pending:', pending);
-                for (let i = 0; i < pending.length; i++) {
-                    let keys = Object.keys(pending[i]);
-                    console.log('current pending:', pending[i], keys);
-                    for (let j = 0; j < keys.length; j++) {
-                        if (
-                            pending[i][keys[j]] !== settledPrescription[keys[j]]
-                        ) {
-                            break;
-                        }
-                        if (j === keys.length - 1) {
-                            index = i;
-                        }
-                    }
-                    if (index) {
-                        break;
-                    }
-                }
-                // console.table(
-                //   'Checking progress:',
-                //   index,
-                //   prescription,
-                //   settledPrescription,
+  // console.log('pers', prescription);
 
-                //   pending
-                // );
-                setPending([
-                    ...pending.slice(0, index),
-                    ...pending.slice(index + 1),
-                ]);
-                setPrescription(
-                    [...pending.slice(0, index), ...pending.slice(index + 1)][0]
-                );
-                setSend(false);
-                setReason('');
-                setInteractedDrugs([]);
-                setSettledPrescription({});
-            }
-        }
-        if (Object.keys(settledPrescription).length !== 0) {
-            // console.log('updating pending duie to click');
-            updatePending();
-        }
-    }, [
-        settledPrescription,
-        prescription,
-        pending,
-        reason,
-        send,
-        interactedDrugs.length,
-    ]);
+  const handleClose = (value) => {
+    setOpen(false);
+  };
 
-    // console.log('pers', prescription);
+  function handleOpen(item) {
+    setPrescription({ ...item });
+  }
+  function handleDecision(approval) {
+    // console.log('in handle decision');
+    setSettledPrescription({ ...prescription, approved: approval });
+  }
 
-    const handleClose = (value) => {
-        setOpen(false);
+  function ChildModal() {
+    // const [open, setOpen] = React.useState(false);
+    const [reasonText, setReasonText] = React.useState('');
+    const style = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: '500px',
+      bgcolor: 'background.paper',
+      border: '2px solid #000',
+      boxShadow: 24,
+      pt: 2,
+      px: 4,
+      pb: 3,
     };
-
     function validate(e) {
       setReasonValidate(e.target.value);
     }
@@ -291,166 +225,41 @@ export default function PendingDialog({ open, setOpen }) {
       setOpenStatusC(false);
       setSend(true);
     }
+    // const handleOpen = () => {
+    //   setOpen(true);
+    // };
+    const handleCloseChild = () => {
+      setOpenStatusC(false);
+      // setOpen(false);
+    };
 
-    function ChildModal() {
-        // const [open, setOpen] = React.useState(false);
-        const [reasonText, setReasonText] = React.useState('');
-        const style = {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: '500px',
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: 24,
-            pt: 2,
-            px: 4,
-            pb: 3,
-        };
-        function handleOverrideChange(e) {
-            setReasonText(e.target.value);
-        }
-        function handleOverrideClick() {
-            // console.log(document.querySelector('#drugInteractionOverride').value);
-            setReason(document.querySelector('#drugInteractionOverride').value);
-            setOpenStatusC(false);
-            setSend(true);
-        }
-        // const handleOpen = () => {
-        //   setOpen(true);
-        // };
-        const handleCloseChild = () => {
-            setOpenStatusC(false);
-            // setOpen(false);
-        };
-
-        return (
-            <React.Fragment>
-                {/* <Button >Open Child Modal</Button> */}
-                <Modal
-                    hideBackdrop
-                    open={openStatusC}
-                    onClose={handleCloseChild}
-                    aria-labelledby='child-modal-title'
-                    aria-describedby='child-modal-description'
-                >
-                    <Box sx={{ ...style }}>
-                        {!prescription ? (
-                            <></>
-                        ) : (
-                            <h2 id='child-modal-title'>
-                                WARNING: There is a severe interaction between{' '}
-                                {prescription.name} and other drugs{' '}
-                                {prescription.firstname} {prescription.surname}{' '}
-                                is currently prescribed{' '}
-                                {interactedDrugs.length === 0 ? (
-                                    <></>
-                                ) : (
-                                    interactedDrugs.reduce(
-                                        (curr, prev) => curr + ', ' + prev
-                                    )
-                                )}
-                            </h2>
-                        )}
-
-                        <p id='child-modal-description'>
-                            If you want to continue with this prescription
-                            please provide a valid reason below:
-                        </p>
-                        <TextField
-                            autoFocus
-                            margin='dense'
-                            id='drugInteractionOverride'
-                            label='Reason to continue prescription'
-                            type='text'
-                            name='interactionReason'
-                            fullWidth
-                            variant='standard'
-                            onChange={handleOverrideChange}
-                            error={reasonText ? false : true}
-                            required
-                        />
-                        <div
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'space-evenly',
-                            }}
-                        >
-                            <Button onClick={handleCloseChild}>Cancel</Button>
-                            <Button
-                                onClick={handleOverrideClick}
-                                disabled={reasonText ? false : true}
-                            >
-                                Confirm Prescription
-                            </Button>
-                        </div>
-                    </Box>
-                </Modal>
-            </React.Fragment>
-        );
-    }
     return (
-
       <React.Fragment>
         {/* <Button >Open Child Modal</Button> */}
         <Modal
           hideBackdrop
           open={openStatusC}
           onClose={handleCloseChild}
-          aria-labelledby='child-modal-title'
-          aria-describedby='child-modal-description'
+          aria-labelledby="child-modal-title"
+          aria-describedby="child-modal-description"
         >
           <Box sx={{ ...style }}>
             {!prescription ? (
               <></>
             ) : (
-              <h2 id='child-modal-title'>
+              <h2 id="child-modal-title">
                 WARNING: There is a severe interaction between{' '}
                 {prescription.name} and other drugs {prescription.firstname}{' '}
                 {prescription.surname} is currently prescribed{' '}
                 {interactedDrugs.length === 0 ? (
                   <></>
                 ) : (
-                    <div className='pendingBox'>
-                        <section>
-                            {' '}
-                            <DialogTitle>Pending Prescriptions</DialogTitle>
-                            <>
-                                {' '}
-                                {pending.map((item) => {
-                                    return (
-                                        <Typography
-                                            key={uuidv4()}
-                                            onClick={() => {
-                                                handleOpen(item);
-                                            }}
-                                        >
-                                            {item.firstname} {item.surname}{' '}
-                                            {item.name}
-                                        </Typography>
-                                    );
-                                })}
-                            </>
-                        </section>
-                        <section>
-                            <h4>
-                                {prescription.firstname} {prescription.surname}{' '}
-                                {prescription.date}
-                            </h4>
-                            <div>
-                                {prescription.name} {'-'}
-                                {prescription.quantity + ', '}
-                                {prescription.dosage}
-                                {prescription.measurement}
-                                {', '}
-                                {prescription.frequency}
-                                {', '}
-                                {prescription.total + ' total'}
-                                {' (' + prescription.type + ')'}
-                            </div>
+                  interactedDrugs.reduce((curr, prev) => curr + ', ' + prev)
+                )}
+              </h2>
+            )}
 
-            <p id='child-modal-description'>
+            <p id="child-modal-description">
               If you want to continue with this prescription please provide a
               valid reason below:
             </p>
@@ -510,23 +319,16 @@ export default function PendingDialog({ open, setOpen }) {
           }}
         />
       ) : null}
-      <Dialog onClose={handleClose} open={open} maxWidth='lg' fullWidth>
+      <Dialog onClose={handleClose} open={open} maxWidth="lg" fullWidth>
         {pending.length === 0 ? (
           <div>No current pending prescriptions</div>
         ) : (
-          <div className='pendingBox'>
-            <section className='pending-patients'>
-              <DialogTitle
-                sx={{
-                  fontSize: '1.1em',
-                  fontWeight: 'bold',
-                  marginLeft: '-1.3em',
-                  fontFamily: 'Inter, sans-serif',
-                }}
-              >
-                Prescriptions Pending for:
-              </DialogTitle>
+          <div className="pendingBox">
+            <section>
+              {' '}
+              <DialogTitle>Pending Prescriptions</DialogTitle>
               <>
+                {' '}
                 {pending.map((item) => {
                   return (
                     <Typography
@@ -541,8 +343,8 @@ export default function PendingDialog({ open, setOpen }) {
                 })}
               </>
             </section>
-            <section className='pending-item'>
-              <h4 className='pending-title'>
+            <section>
+              <h4>
                 {prescription.firstname} {prescription.surname}{' '}
                 {prescription.date}
               </h4>
@@ -573,7 +375,7 @@ export default function PendingDialog({ open, setOpen }) {
                 <></>
               )}
               <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                {/* <button
+                <button
                   onClick={() => {
                     handleDecision(true);
                   }}
@@ -588,20 +390,7 @@ export default function PendingDialog({ open, setOpen }) {
                 >
                   {' '}
                   Deny
-                </button> */}
-                <ButtonComponent
-                  text1={'Approve'}
-                  onClick={() => {
-                    handleDecision(true);
-                  }}
-                />
-                <ButtonComponent
-                  text1={'Deny'}
-                  onClick={() => {
-                    handleDecision(false);
-                  }}
-                />
-                <ButtonComponent text1={'Close'} onClick={handleClose} />
+                </button>
               </div>
             </section>
           </div>
